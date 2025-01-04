e3f92e33e32e52481545f2557eb304f1d109c3bc
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2025-01-04 10:03:08 -0500

    High precision benchmarks

---
+++ benchmarks/max_precision/decades.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+make precision-10pwr0.scount P=0
+make precision-10pwr1.scount P=1
+make precision-10pwr2.scount P=2
+make precision-10pwr3.scount P=3
+make precision-10pwr4.scount P=4
+make precision-10pwr5.scount P=5
+make precision-10pwr6.scount P=6
+make precision-10pwr7.scount P=7
+
+
