0e902885a8252a99a02d02a8cc66fe8156971857
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2025-01-07 15:00:58 -0500

    Minor tweaks

--- src/arithmetic/analysis.c
+++ src/arithmetic/analysis.c
@@ -33,34 +33,13 @@ static bool double_is_nan(double x) {
 
 
 double digit_precision_q25(q25_ptr qx, q25_ptr qy) {
-#if DEBUG
-    {
-	char *sqx = q25_string(qx);
-	char *sqy = q25_string(qy);
-
-	printf("Finding digit precision for q25 numbers %s : %s.", sqx, sqy);
-	free(sqx); free(sqy);
-    }
-#endif
     if (!q25_is_valid(qx) || q25_is_infinite(qx, NULL)
-	|| !q25_is_valid(qy) || q25_is_infinite(qy, NULL)) {
-#if DEBUG
-	printf("  Invalid arguments.  Return -1.0\n");
-#endif
+	|| !q25_is_valid(qy) || q25_is_infinite(qy, NULL))
 	return -1.0;
-    }
-    if (q25_compare(qx, qy) == 0) {
-#if DEBUG
-	printf("  Strict equality.  Return %d\n", MAX_DIGIT_PRECISION);
-#endif
+    if (q25_compare(qx, qy) == 0)
 	return MAX_DIGIT_PRECISION;
-    }
-    if (q25_is_zero(qx) || q25_is_zero(qy)) {
-#if DEBUG
-	printf("  One argument is zero.  Return 0.0\n");
+    if (q25_is_zero(qx) || q25_is_zero(qy))
 	return 0.0;
-#endif	
-    }
     int pos = q25_enter();
     q25_ptr aqx = q25_is_negative(qx) ? q25_mark(q25_negate(qx)) : qx;
     q25_ptr aqy = q25_is_negative(qy) ? q25_mark(q25_negate(qy)) : qy;
@@ -72,9 +51,6 @@ double digit_precision_q25(q25_ptr qx, q25_ptr qy) {
     /* See whether over limit */
     q25_ptr dscale = q25_scale(denom, -MAX_DIGIT_PRECISION, -MAX_DIGIT_PRECISION);
     if (q25_compare(num, dscale) < 0) {
-#if DEBUG
-	printf("  Very small precision.  Return %d\n", MAX_DIGIT_PRECISION);
-#endif
 	q25_leave(pos);
 	return (double) MAX_DIGIT_PRECISION;
     }
@@ -86,10 +62,6 @@ double digit_precision_q25(q25_ptr qx, q25_ptr qy) {
 	scale10 = -(mag_num > mag_denom ? mag_denom : mag_num);
     else if (mag_num < 0 && mag_denom < 0)
 	scale10 = -(mag_num < mag_denom ? mag_denom : mag_num);
-#if DEBUG
-    printf("   Found log10(num) ~= %d, log10(denom) ~= %d.  Scaling by 10^%d\n",
-	   mag_num, mag_denom, scale10);
-#endif    
     q25_inplace_scale(num, scale10, scale10);
     q25_inplace_scale(denom, scale10, scale10);
     /* Must convert here.  Risks over/underflow */
@@ -97,24 +69,13 @@ double digit_precision_q25(q25_ptr qx, q25_ptr qy) {
     double rdenom = q25_to_double(denom);
 
     q25_leave(pos);
-    if (rdenom == 0 || double_is_infinite(rnum, NULL) || double_is_infinite(rdenom, NULL)) {
-#if DEBUG
-	printf("  Encountered problem converting to double.  Got rnum = %f, rdenom = %f\n", rnum, rdenom);
-#endif
+    if (rdenom == 0 || double_is_infinite(rnum, NULL) || double_is_infinite(rdenom, NULL))
 	return -1.0;
-    }
     if (rnum == 0) {
-	if (rdenom == 0) {
-#if DEBUG
-	    printf("  Zero denominator and numerator.  Return -1.0\n");
-#endif
+	if (rdenom == 0)
 	    return -1.0;
-	} else {
-#if DEBUG
-	    printf("  Nonzero denominator and Zero numerator.  Return %d\n", MAX_DIGIT_PRECISION);
-#endif
+	else
 	    return MAX_DIGIT_PRECISION;
-	}
     }
     double div = rnum/rdenom;
     double ldiv = -log10(div);
@@ -122,10 +83,6 @@ double digit_precision_q25(q25_ptr qx, q25_ptr qy) {
 	ldiv = 0.0;
     if (ldiv > MAX_DIGIT_PRECISION)
 	ldiv = (double) MAX_DIGIT_PRECISION;
-#if DEBUG
-    printf("  Normal case. Num = %g, Denom = %g, Ratio = %g.  Return %.4f\n", 
-	   rnum, rdenom, div, ldiv);
-#endif
     return ldiv;
 }
 
