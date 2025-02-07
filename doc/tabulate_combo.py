3fe43b83983527d88b2f719af1e501966b48703c
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2025-02-07 08:13:36 -0500

    Account for time spent in failed runs of MPQ

--- doc/tabulate_combo.py
+++ doc/tabulate_combo.py
@@ -40,6 +40,16 @@ def err(fatal, msg):
     else:
         sys.stderr.write("WARNING: %s\n" % msg)
 
+# Hack to account for times taken by failed runs
+fail_time_dict = {"mc2024_track2-random_023" : 1592.0, "mc2024_track2-random_178" : 1634.2 }
+
+def fail_time(instance):
+    formula = instance[:-4]
+    if formula not in fail_time_dict:
+        print("Couldn't find entry for instance %s (formula %s)" % (instance, formula))
+        return 0.0
+    return fail_time_dict[formula]
+
 class Instance:
     name = None
     method = None
@@ -47,6 +57,7 @@ class Instance:
     mpfi_seconds = None
     mpfi2_seconds = None
     mpq_seconds = None
+    mpq_ok = False
     
     def __init__(self, dict):
         self.name = dict["bench"]
@@ -79,8 +90,10 @@ class Instance:
         self.mpq_seconds = None
         try:
             self.mpq_seconds = float(smpq)
+            self.mpq_ok = True
         except:
-            pass
+            self.mpq_seconds = fail_time(self.name)
+            self.mpq_ok = False
         if self.method == method_mpf and self.mpf_seconds is None: 
             err(True, "Instance %s.  Need MPF seconds" % str(self))
         elif self.method == method_mpfi and self.mpfi_seconds is None:
@@ -91,7 +104,7 @@ class Instance:
             err(True, "Instance %s.  Need MPFI, MPFI2, and MPQ seconds" % str(self))
 
     def __str__(self):
-        return "I.%s (%s) MPF:%s MPFI:%s MPFI2:%s MPQ:%s" % (self.name, method_name[self.method], str(self.mpf_seconds), str(self.mpfi_seconds), str(self.mpfi2_seconds), str(self.mpq_seconds))
+        return "I.%s (%s) MPF:%s MPFI:%s MPFI2:%s MPQ(%s):%s" % (self.name, method_name[self.method], str(self.mpf_seconds), str(self.mpfi_seconds), str(self.mpfi2_seconds), "S" if self.mpq_ok else "F", str(self.mpq_seconds))
 
 instances = []
 average_time = [0] * method_count
@@ -118,97 +131,120 @@ def load(verbose):
 
 def tabulate(target_method, label):
     global average_time
-    count = [0] * 4
+    ok_count = [0] * 4
+    fail_count = [0] * 4
     time = [0] * 4
-    total_count = 0
+    total_ok_count = 0
+    total_fail_count = 0
     total_time = 0
     for instance in instances:
         if target_method == method_mpq:
-            if instance.mpq_seconds is not None:
-                count[method_mpq] += 1
-                time[method_mpq] += instance.mpq_seconds
-                total_count += 1
-                total_time += instance.mpq_seconds
+            time[method_mpq] += instance.mpq_seconds
+            total_time += instance.mpq_seconds
+            if instance.mpq_ok:
+                ok_count[method_mpq] += 1
+                total_ok_count += 1
+            else:
+                fail_count[method_mpq] += 1
+                total_fail_count += 1
         elif target_method == method_mpf:
             if instance.method == method_mpf:
-                count[method_mpf] += 1
+                ok_count[method_mpf] += 1
                 time[method_mpf] += instance.mpf_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpf_seconds
-            elif instance.mpq_seconds is not None:
-                count[method_mpq] += 1
+            else:
                 time[method_mpq] += instance.mpq_seconds
-                total_count += 1
                 total_time += instance.mpq_seconds
+                if instance.mpq_ok:
+                    ok_count[method_mpq] += 1
+                    total_ok_count += 1
+                else:
+                    fail_count[method_mpq] += 1
+                    total_fail_count += 1
         elif target_method == method_mpfi:
             if instance.method == method_mpf:
-                count[method_mpf] += 1
+                ok_count[method_mpf] += 1
                 time[method_mpf] += instance.mpf_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpf_seconds
             elif instance.method == method_mpfi:
-                count[method_mpfi] += 1
+                ok_count[method_mpfi] += 1
                 time[method_mpfi] += instance.mpfi_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpfi_seconds
             elif instance.method == method_mpfi2:
-                count[method_mpfi] += 1
+                fail_count[method_mpfi] += 1
                 time[method_mpfi] += instance.mpfi_seconds
                 total_time += instance.mpfi_seconds
-                if instance.mpq_seconds is not None:
-                    count[method_mpq] += 1
-                    time[method_mpq] += instance.mpq_seconds
-                    total_count += 1
-                    total_time += instance.mpq_seconds
-            elif instance.mpq_seconds is not None:
-                count[method_mpfi] += 1
-                time[method_mpfi] += instance.mpfi_seconds
-                total_time += instance.mpfi_seconds
-                count[method_mpq] += 1
                 time[method_mpq] += instance.mpq_seconds
-                total_count += 1
                 total_time += instance.mpq_seconds
+                if instance.mpq_seconds is not None:
+                    ok_count[method_mpq] += 1
+                    total_ok_count += 1
+                else:
+                    fail_count[method_mpq] += 1
+                    total_fail_count += 1
+            else:
+                fail_count[method_mpfi] += 1
+                time[method_mpfi] += instance.mpfi_seconds
+                total_time += instance.mpfi_seconds
+                time[method_mpq] += instance.mpq_seconds
+                total_time += instance.mpq_seconds
+                if instance.mpq_seconds is not None:
+                    ok_count[method_mpq] += 1
+                    total_ok_count += 1
+                else:
+                    fail_count[method_mpq] += 1
+                    total_fail_count += 1
         elif target_method == method_mpfi2:
             if instance.method == method_mpf:
-                count[method_mpf] += 1
+                ok_count[method_mpf] += 1
                 time[method_mpf] += instance.mpf_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpf_seconds
             elif instance.method == method_mpfi:
-                count[method_mpfi] += 1
+                ok_count[method_mpfi] += 1
                 time[method_mpfi] += instance.mpfi_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpfi_seconds
             elif instance.method == method_mpfi2:
-                count[method_mpfi] += 1
+                fail_count[method_mpfi] += 1
                 time[method_mpfi] += instance.mpfi_seconds
                 total_time += instance.mpfi_seconds
-                count[method_mpfi2] += 1
+                ok_count[method_mpfi2] += 1
                 time[method_mpfi2] += instance.mpfi2_seconds
-                total_count += 1
+                total_ok_count += 1
                 total_time += instance.mpfi2_seconds
-            elif instance.mpq_seconds is not None:
-                count[method_mpfi] += 1
+            else:
+                fail_count[method_mpfi] += 1
                 time[method_mpfi] += instance.mpfi_seconds
                 total_time += instance.mpfi_seconds
-                count[method_mpfi2] += 1
+                fail_count[method_mpfi2] += 1
                 time[method_mpfi2] += instance.mpfi2_seconds
                 total_time += instance.mpfi2_seconds
-                count[method_mpq] += 1
                 time[method_mpq] += instance.mpq_seconds
-                total_count += 1
                 total_time += instance.mpq_seconds
+                if instance.mpq_seconds is not None:
+                    ok_count[method_mpq] += 1
+                    total_ok_count += 1
+                else:
+                    fail_count[method_mpq] += 1
+                    total_fail_count += 1
     fields_top = [label, "Runs"]
     fields_bottom = ["", "Hours"]
     for comp_method in range(method_count):
         mtime = time[comp_method]/3600.0
-        fields_top += ["" if count[comp_method] == 0 else str(count[comp_method])]
+        fields_top += ["" if mtime == 0.0 else str(ok_count[comp_method]) + "+" + str(fail_count[comp_method])]
         fields_bottom +=  ["" if mtime == 0.0 else "%.2f" % mtime]
+    total_count = total_ok_count + total_fail_count
     avg = total_time/total_count
     average_time[target_method] = avg
     ttime = total_time/3600.0
-    fields_top += [str(total_count), "%.1f" % avg]
-    fields_bottom += ["%.2f" % ttime, ""]
+    fields_top += [str(total_ok_count) + "+" + str(total_fail_count)]
+    fields_bottom += ["%.2f" % ttime]
+#    fields_top += ["%.1f" % avg]
+#    fields_bottom += [""]
     return (fields_top, fields_bottom)
 
 def table_entry(fields):
