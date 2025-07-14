52e1c321e389f62662ec24858a81fb4c1208fdce
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2025-07-14 18:59:32 -0400

    Tuning

--- src/arithmetic/erd.h
+++ src/arithmetic/erd.h
@@ -34,7 +34,7 @@
    ERDZ:        0.0 has exp = 0
 
 */
-#define ERDZ 0
+#define ERDZ 1
 
 #if ERDZ
 #define ZEXP 0
@@ -113,13 +113,27 @@ static double dbl_assemble(uint64_t sign, int64_t exp, uint64_t frac) {
 }
 
 static double dbl_replace_exponent(double x, int64_t exp) {
-    uint64_t bexp = (uint64_t) ((exp + DBL_BIAS) & DBL_EXP_MASK) << DBL_EXP_OFFSET;
+    uint64_t bexp = (uint64_t) (exp + DBL_BIAS);
+#if !ERDZ
+    bexp &= DBL_EXP_MASK;
+#endif
+    bexp = bexp << DBL_EXP_OFFSET;
     uint64_t bx = dbl_get_bits(x);
     uint64_t mask = ~(DBL_EXP_MASK << DBL_EXP_OFFSET);
     bx &= mask;
     bx += bexp;
     return dbl_from_bits(bx);
 }
+
+static double dbl_zero_exponent(double x) {
+    uint64_t bexp = (uint64_t) DBL_BIAS << DBL_EXP_OFFSET;
+    uint64_t bx = dbl_get_bits(x);
+    uint64_t mask = ~(DBL_EXP_MASK << DBL_EXP_OFFSET);
+    bx &= mask;
+    bx += bexp;
+    return dbl_from_bits(bx);
+}
+
 
 static double dbl_infinity(int sign) {
     return dbl_assemble(sign, DBL_EXP_MASK - DBL_BIAS, 0);
@@ -145,7 +159,7 @@ static erd_t erd_normalize(erd_t a) {
 	return erd_zero();
     erd_t nval;
     nval.exp = a.exp + dbl_get_exponent(a.dbl);
-    nval.dbl = dbl_replace_exponent(a.dbl, 0);
+    nval.dbl = dbl_zero_exponent(a.dbl);
     return nval;
 }
 
@@ -164,8 +178,10 @@ erd_t erd_from_mpf(mpf_srcptr fval) {
     erd_t nval;
     long int exp;
     nval.dbl = mpf_get_d_2exp(&exp, fval);
+#if !ERDZ
     if (nval.dbl == 0)
 	return erd_zero();
+#endif
     nval.exp = (int64_t) exp;
     return erd_normalize(nval);
 }
@@ -243,7 +259,9 @@ static erd_t erd_mul_seq_slow(erd_t *val, int len) {
 }
 
 static erd_t erd_mul_seq_x1(erd_t *val, int len) {
-    erd_t result = len == 0 ? erd_from_double(1.0) : val[0];
+    if (len == 0)
+	return erd_from_double(1.0);
+    erd_t result = val[0];
     int i;
     int count = 1;
     for (i = 1; i < len; i++) {
@@ -254,6 +272,33 @@ static erd_t erd_mul_seq_x1(erd_t *val, int len) {
 	    result = erd_normalize(result);
 	}
     }
+    return erd_normalize(result);
+}
+
+static erd_t erd_mul_seq_x2(erd_t *val, int len) {
+    // Assume len >= 2
+    erd_t prod[2];
+    int i, j;
+    for (j = 0; j < 2; j++) 
+	prod[j] = val[j];
+    int count = 0;
+    for (i = 2; i <= len-2; i+= 2) {
+	for (j = 0; j < 2; j++)
+	    prod[j] = erd_quick_mul(prod[j], val[i+j]);
+	if (++count > MAX_MUL) {
+	    count = 0;
+	    for (j = 0; j < 2; j++)
+		prod[j] = erd_normalize(prod[j]);
+	}
+    }
+    if (count * 2 > MAX_MUL) {
+	for (j = 0; j < 2; j++)
+	    prod[j] = erd_normalize(prod[j]);
+    }
+
+    erd_t result = erd_quick_mul(prod[0], prod[1]);
+    for (; i < len; i++)
+	result = erd_quick_mul(prod[0], val[i]);
     return erd_normalize(result);
 }
 
@@ -278,19 +323,20 @@ static erd_t erd_mul_seq_x4(erd_t *val, int len) {
 	    prod[j] = erd_normalize(prod[j]);
     }
 
-    prod[2] = erd_quick_mul(prod[2], prod[3]);
-    prod[1] = erd_quick_mul(prod[1], prod[2]);
-    prod[0] = erd_quick_mul(prod[0], prod[1]);
+
+    erd_t result = prod[0];
+    for (j = 1; j < 4; j++)
+	result = erd_quick_mul(result, prod[j]);
     for (; i < len; i++)
-	prod[0] = erd_quick_mul(prod[0], val[i]);
-    return erd_normalize(prod[0]);
+	result = erd_quick_mul(result, val[i]);
+    return erd_normalize(result);
 }
 
 /* Compute product of sequence of values */
 erd_t erd_mul_seq(erd_t *val, int len) {
-    if (len < 4)
+    if (len < 8)
 	return erd_mul_seq_x1(val, len);
-    return erd_mul_seq_x4(val, len);
+    return erd_mul_seq_x2(val, len);
 }
 
 erd_t erd_div(erd_t a, erd_t b) {
